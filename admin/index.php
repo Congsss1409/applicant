50563a5220a872e0d1129a3d10633820a0131e3d
Congsss1409 <marcotaba1121@gmail.com>
2024-05-10 11:54:10 +0800

    offline

--- admin/index.php
+++ admin/index.php
@@ -227,6 +227,6 @@ echo "</div>";
     </div>
 
     <!-- Bootstrap 5 JavaScript (use your own preferred version) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html>
